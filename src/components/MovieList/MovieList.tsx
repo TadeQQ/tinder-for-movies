02d1d4a143c685fcdb1628afa7d8e293714ecb44
TadeQ <tadeuszkiszka1@gmail.com>
2023-03-14 18:14:56 +0100

    feat: style Movielist

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,30 +1,39 @@
 import { useState, useContext } from 'react';
-import TinderCard from 'react-tinder-card';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { MovieContext } from '../../hooks/Context/MovieContext';
-import axios from 'axios';
+import { Button } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 export const MovieList = () => {
-  const { movies, loading, error } = useContext(MovieContext);
-  const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+  const { movies, loading, error, status, acceptMovie, rejectMovie } =
+    useContext(MovieContext);
+  const [currentMovieIndex, setCurrentMovieIndex] = useState<number>(0);
 
   const handleAccept = () => {
     const currentMovieId = movies[currentMovieIndex]?.id;
-    console.log(currentMovieId);
-    // if (currentMovieId) {
-    //   axios.put(`/recommendations/${currentMovieId}/accept`);
-    // }
-
+    // acceptMovie(currentMovieId);
     setCurrentMovieIndex((prevIndex) => prevIndex + 1);
+    console.log('accepted');
   };
 
   const handleReject = () => {
     const currentMovieId = movies[currentMovieIndex]?.id;
-    console.log(currentMovieId);
-    // if (currentMovieId) {
-    //   axios.put(`/recommendations/${currentMovieId}/reject`);
-    // }
-
+    // rejectMovie(currentMovieId);
     setCurrentMovieIndex((prevIndex) => prevIndex + 1);
+    console.log('rejected');
   };
+
+  const handleError: React.ReactEventHandler<HTMLImageElement> = (e) => {
+    e.currentTarget.onerror = null;
+    e.currentTarget.src =
+      'https://images.unsplash.com/photo-1572700432881-42c60fe8c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80';
+  };
+
+  const lastMovie = currentMovieIndex === movies?.length - 1;
 
   if (loading) {
     return <div>Loading...</div>;
@@ -33,39 +42,51 @@ export const MovieList = () => {
   if (error) {
     return <div>{error}</div>;
   }
-  const handleError: React.ReactEventHandler<HTMLImageElement> = (e) => {
-    e.currentTarget.onerror = null;
-    e.currentTarget.src =
-      'https://images.unsplash.com/photo-1572700432881-42c60fe8c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80';
-  };
 
   return (
-    <>
-      <div key={movies[currentMovieIndex]?.id}>
-        <div>
-          {movies[currentMovieIndex]?.title} {movies[currentMovieIndex]?.rating}
-        </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia>
         <img
           alt={movies[currentMovieIndex]?.title}
           src={movies[currentMovieIndex]?.imageURL}
-          width={300}
-          height={450}
+          width={345}
+          height={490}
           onError={handleError}
         />
-      </div>
-      <div>{movies[currentMovieIndex]?.summary}</div>
-      <div>
-        {currentMovieIndex === movies?.length - 1 ? (
-          <button disabled>Accept</button>
-        ) : (
-          <button onClick={handleAccept}>Accept</button>
-        )}
-        {currentMovieIndex === movies?.length - 1 ? (
-          <button disabled>Reject</button>
-        ) : (
-          <button onClick={handleReject}>Reject</button>
-        )}
-      </div>
-    </>
+      </CardMedia>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {movies[currentMovieIndex]?.title} {movies[currentMovieIndex]?.rating}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {movies[currentMovieIndex]?.summary}
+        </Typography>
+      </CardContent>
+
+      <CardActions
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Button
+          variant="contained"
+          color="success"
+          disabled={lastMovie}
+          onClick={handleAccept}
+        >
+          Accept
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          disabled={lastMovie}
+          onClick={handleReject}
+        >
+          Reject
+        </Button>
+      </CardActions>
+    </Card>
   );
 };

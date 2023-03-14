095336c7ccd75e7bdbbcf78f82abb2e6974ee242
TadeQ <tadeuszkiszka1@gmail.com>
2023-03-14 19:39:50 +0100

    refactor: copy code from recommendations to index

---
+++ src/pages/api/recommendations/index.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { recommendations } from '../../../../data/db';
+
+interface Recommendation {
+  id: string;
+  imageURL: string;
+  title: string;
+  summary: string;
+  rating: number;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ recommendations: Recommendation[] }>
+) {
+  res.status(200).json(recommendations);
+}

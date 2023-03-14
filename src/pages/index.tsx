0ac14d7900c65862d370c51f00f224c238665261
TadeQ <tadeuszkiszka1@gmail.com>
2023-03-14 01:19:52 +0100

    feat: change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import { Card } from '@/components/Card/Card';
+import { MovieList } from '@/components/MovieList/MovieList';
+import { MovieCard } from '@/components/Card';
 import Head from 'next/head';
 export default function Home() {
   return (
     <>
       <header>
-        <div>Home</div>
+        <div>Tinder for movies</div>
       </header>
-      <main>
-        <Card />
-      </main>
+      <div>
+        <MovieCard />
+      </div>
     </>
   );
 }

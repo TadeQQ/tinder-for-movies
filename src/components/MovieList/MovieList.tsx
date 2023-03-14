58fe873fb800c0bd8e5edd975cdb13de23588a68
TadeQ <tadeuszkiszka1@gmail.com>
2023-03-14 01:20:43 +0100

    feat: only one film is visible and buttons

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,8 +1,30 @@
-import { useContext } from 'react';
-import { MovieContext } from '../Context/MovieContext';
-
+import { useState, useContext } from 'react';
+import TinderCard from 'react-tinder-card';
+import { MovieContext } from '../../hooks/Context/MovieContext';
+import axios from 'axios';
 export const MovieList = () => {
   const { movies, loading, error } = useContext(MovieContext);
+  const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+
+  const handleAccept = () => {
+    const currentMovieId = movies[currentMovieIndex]?.id;
+    console.log(currentMovieId);
+    // if (currentMovieId) {
+    //   axios.put(`/recommendations/${currentMovieId}/accept`);
+    // }
+
+    setCurrentMovieIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handleReject = () => {
+    const currentMovieId = movies[currentMovieIndex]?.id;
+    console.log(currentMovieId);
+    // if (currentMovieId) {
+    //   axios.put(`/recommendations/${currentMovieId}/reject`);
+    // }
+
+    setCurrentMovieIndex((prevIndex) => prevIndex + 1);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -11,12 +33,39 @@ export const MovieList = () => {
   if (error) {
     return <div>{error}</div>;
   }
-  console.log(movies);
+  const handleError: React.ReactEventHandler<HTMLImageElement> = (e) => {
+    e.currentTarget.onerror = null;
+    e.currentTarget.src =
+      'https://images.unsplash.com/photo-1572700432881-42c60fe8c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80';
+  };
+
   return (
     <>
-      MovieList:
-      {Array.isArray(movies) &&
-        movies?.map((movie) => <div key={movie.id}>{movie.title}</div>)}
+      <div key={movies[currentMovieIndex]?.id}>
+        <div>
+          {movies[currentMovieIndex]?.title} {movies[currentMovieIndex]?.rating}
+        </div>
+        <img
+          alt={movies[currentMovieIndex]?.title}
+          src={movies[currentMovieIndex]?.imageURL}
+          width={300}
+          height={450}
+          onError={handleError}
+        />
+      </div>
+      <div>{movies[currentMovieIndex]?.summary}</div>
+      <div>
+        {currentMovieIndex === movies?.length - 1 ? (
+          <button disabled>Accept</button>
+        ) : (
+          <button onClick={handleAccept}>Accept</button>
+        )}
+        {currentMovieIndex === movies?.length - 1 ? (
+          <button disabled>Reject</button>
+        ) : (
+          <button onClick={handleReject}>Reject</button>
+        )}
+      </div>
     </>
   );
 };

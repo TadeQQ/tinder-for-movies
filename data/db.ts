197ffae9671daf8936d4c111cadcb2ed2c0051a7
TadeQ <tadeuszkiszka1@gmail.com>
2023-03-14 18:13:24 +0100

    refactor: create db.ts

---
+++ data/db.ts
@@ -0,0 +1,116 @@
+export const recommendations = {
+  recommendations: [
+    {
+      id: '1and3011',
+      imageURL:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+      title: 'Inferno',
+      summary: 'Lorem ipsum….',
+      rating: 5.3,
+    },
+    {
+      id: '2301abc',
+      imageURL:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg',
+      title: 'Star Wars: Episode VII - The Force Awakens',
+      summary: 'Lorem ipsum….',
+      rating: 8.2,
+    },
+    {
+      id: '3045abc',
+      imageURL:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTIxNzIxMTUxMl5BMl5BanBnXkFtZTcwNzIyNzUyMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+      title: 'The Dark Knight',
+      summary: 'Lorem ipsum….',
+      rating: 9.0,
+    },
+    {
+      id: '4080abc',
+      imageURL:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTIyMTIxNDYwN15BMl5BanBnXkFtZTcwNjcyNTI2Mw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+      title: 'The Shawshank Redemption',
+      summary: 'Lorem ipsum….',
+      rating: 9.3,
+    },
+    {
+      id: '5015abc',
+      imageURL:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5NDI2NzM5Ml5BMl5BanBnXkFtZTgwNjIyODU4MTE@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+      title: 'Jurassic Park',
+      summary: 'Lorem ipsum….',
+      rating: 8.1,
+    },
+    {
+      id: '6030abc',
+      imageURL:
+        'https://m.media-amazon.com/images/I/51W8jfsqMkL._AC_SY741_.jpg',
+      title: 'Forrest Gump',
+      summary:
+        'Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.',
+      rating: 8.8,
+    },
+    {
+      id: '6030def',
+      imageURL:
+        'https://m.media-amazon.com/images/I/51Drl9hN22L._AC_SY741_.jpg',
+      title: 'Inception',
+      summary:
+        'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+      rating: 8.7,
+    },
+    {
+      id: '6030ghi',
+      imageURL:
+        'https://m.media-amazon.com/images/I/51oDs32SXlL._AC_SY741_.jpg',
+      title: 'The Matrix',
+      summary:
+        'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+      rating: 8.7,
+    },
+    {
+      id: '6030jkl',
+      imageURL:
+        'https://m.media-amazon.com/images/I/41zO8WHBxxL._AC_SY741_.jpg',
+      title: 'The Silence of the Lambs',
+      summary:
+        'A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.',
+      rating: 8.6,
+    },
+    {
+      id: '6030mno',
+      imageURL:
+        'https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY741_.jpg',
+      title: 'The Shawshank Redemption',
+      summary:
+        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+      rating: 9.3,
+    },
+    {
+      id: '6030pqr',
+      imageURL:
+        'https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY741_.jpg',
+      title: 'The Green Mile',
+      summary:
+        'The lives of guards on Death Row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.',
+      rating: 8.6,
+    },
+    {
+      id: '6030stu',
+      imageURL:
+        'https://m.media-amazon.com/images/I/61g6+13ZjxL._AC_SY741_.jpg',
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      summary:
+        'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+      rating: 8.8,
+    },
+    {
+      id: '6030vwx',
+      imageURL:
+        'https://m.media-amazon.com/images/I/51EG732BV3L._AC_SY741_.jpg',
+      title: 'Fight Club',
+      summary:
+        'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.',
+      rating: 8.8,
+    },
+  ],
+};
